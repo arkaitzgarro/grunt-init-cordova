92ee0ee1938f6b45043aa4bc3757fe7f406c616f
Elías Alonso <elias@redradix.com>
2013-10-22 18:25:05 +0200

    first commit

---
+++ root/src/js/config.js
@@ -0,0 +1,32 @@
+require.config({
+  paths: {
+    backbone: "../../bower_components/backbone/backbone",
+    jquery: "../../bower_components/jquery/jquery",
+    handlebars: "../../bower_components/handlebars/handlebars",
+    "handlebars.runtime": "../../bower_components/handlebars/handlebars.runtime",
+    underscore: "../../bower_components/underscore/underscore"
+  },
+  shim: {
+    jquery: {
+      exports: "jQuery"
+    },
+    underscore: {
+      exports: "_"
+    },
+    backbone: {
+      deps: [
+        "jquery",
+        "underscore"
+      ],
+      exports: "Backbone"
+    },
+    handlebars: {
+      exports: "Handlebars"
+    }
+  }
+});
+
+/* Require the initial module */
+require(["app"], function() {
+  "use strict";
+});
